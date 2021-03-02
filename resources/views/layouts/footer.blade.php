1374efe8d386aefc2ab448ae4d4cd5e583ac5104
Davide <rinaldi.davide94@gmail.com>
2021-03-02 16:42:59 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,95 @@
+{{-- footer --}}
+    <footer>
+        <div class="container-fluid">
+            <div class="container">
+                <div class="row d-flex justify-content-center">
+                    <div class="footer-card">
+                        <ul class= "list-unstyled">
+                            <li>
+                                <h4>Scopri Deliveroo</h4>
+                            </li>
+                            <li>
+                                <a href="#">Chi siamo</a>
+                            </li>
+                            <li>
+                                <a href="#">PressRoom</a>
+                            </li>
+                            <li>
+                                <a href="#">Il nostro blog</a>
+                            </li>
+                            <li>
+                                <a href="#">Programmazione</a>
+                            </li>
+                            <li>
+                                <a href="#">Design</a>
+                            </li>
+                            <li>
+                                <a href="#">Lavora con noi</a>
+                            </li>
+                            <li>
+                                <a href="#">Diventa nostro partner</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer-card">
+                        <ul class= "list-unstyled">
+                            <li>
+                                <h4>Note Legali</h4>
+                            </li>
+                            <li>
+                                <a href="#">Termini e condizioni</a>
+                            </li>
+                            <li>
+                                <a href="#">Informativa sulla privacy</a>
+                            </li>
+                            <li>
+                                <a href="#">Cookies</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer-card">
+                        <ul class= "list-unstyled">
+                            <li>
+                                <h4>Aiuto</h4>
+                            </li>
+                            <li>
+                                <a href="#">Contatti</a>
+                            </li>
+                            <li>
+                                <a href="#">FAQ</a>
+                            </li>
+                            <li>
+                                <a href="#">Tipi di cucina</a>
+                            </li>
+                            <li>
+                                <a href="#">Mappa del sito</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer-card">
+                        <ul class= "list-unstyled">
+                            <li>
+                                <h4>Porta Deliveroo con te</h4>
+                            </li>
+                            <li>
+                                <a href="#"><img src="img/app-store.png" class="app-store" alt="Scarica da applestore"></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="img/google-play.png" class="play-store" alt="Scarica da google play"></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row d-flex footer-data">
+                    <div class="col social">
+                        <i class="fab fa-facebook"></i>
+                        <i class="fab fa-twitter"></i>
+                        <i class="fab fa-instagram"></i>
+                    </div>
+                    <div class=" col credits d-flex justify-content-end">
+                        <span>© Deliveroo 2021.</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
