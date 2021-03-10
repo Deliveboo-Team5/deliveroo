c32393c7ac0db293540a080a4b3c35ae270b991e
Alessandro Ferraro <ale_ferraro@libero.it>
2021-03-10 09:58:41 +0100

    Add Payments page

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Resources\Restaurant as RestaurantResource;
 use App\Restaurant;
+use Illuminate\Http\Request;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -44,3 +46,58 @@ Route::resource('restaurant', 'RestaurantController')->only('index','show');
  
 Route::get('/api/restaurant', 'RestaurantController@ajaxcall');
 Route::get('/api/food', 'FoodsController@ajaxcall');
+
+
+Route::get('payment', function(){
+    $gateway = new Braintree\Gateway([
+        'environment' => config('services.braintree.environment'),
+        'merchantId' => config('services.braintree.merchantId'),
+        'publicKey' => config('services.braintree.publicKey'),
+        'privateKey' => config('services.braintree.privateKey')
+    ]);
+
+    $token = $gateway->ClientToken()->generate();
+
+    return view('payment', [
+        'token' => $token
+    ]);
+});
+
+Route::post('/checkout', function (Request $request) {
+
+    $gateway = new Braintree\Gateway([
+        'environment' => config('services.braintree.environment'),
+        'merchantId' => config('services.braintree.merchantId'),
+        'publicKey' => config('services.braintree.publicKey'),
+        'privateKey' => config('services.braintree.privateKey')
+    ]);
+
+    $amount = $request->amount;
+    $nonce = $request->payment_method_nonce;
+
+    $result = $gateway->transaction()->sale([
+        'amount' => $amount,
+        'paymentMethodNonce' => $nonce,
+        'options' => [
+            'submitForSettlement' => true
+        ]
+    ]);
+
+    if ($result->success) {
+        $transaction = $result->transaction;
+        //header("Location: " . $baseUrl . "transaction.php?id=" . $transaction->id);
+
+        return back()->with('success_message', 'Transaction successful. The ID is: ' . $transaction->id);
+    } else {
+        $errorString = "";
+
+        foreach($result->errors->deepAll() as $error) {
+            $errorString .= 'Error: ' . $error->code . ": " . $error->message . "\n";
+        }
+
+        //$_SESSION["errors"] = $errorString;
+        //header("Location: " . $baseUrl . "index.php");
+
+        return back()->withErrors('An error occurred with the message: ' . $result->message);   
+    }
+});
