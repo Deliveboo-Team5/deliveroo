6aa89f6c55ab390a35037b57f58cea1a6a199acc
Mirelle Nascimento <mmirellenascimento@gmail.com>
2021-03-16 10:26:20 +0100

    Restaurant Seeder

--- database/factories/foodFactory.php
+++ database/factories/foodFactory.php
@@ -10,7 +10,7 @@ $factory->define(Food::class, function (Faker $faker) {
         'name_food' => $faker->word(),
         'price' => $faker->randomFloat($nbMaxDecimals = 2, $min = 4, $max = 40),
         'ingredients' => $faker->sentence($nbWords = 10, $variableNbWords = true),
-        'restaurant_id' => $faker->numberBetween($min = 1, $max = 50),
+        'restaurant_id' => $faker->numberBetween($min = 1, $max = 12),
         'is_visible' => $faker->boolean($chanceOfGettingTrue = 100),
         'img' => "https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg"
     ];
